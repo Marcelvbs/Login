9e4d5828818cca7003f9604d22cef60c9f182f98
marcelvbs <celvbs@gmail.com>
2022-02-23 14:01:56 -0300

    Adicionar arquivos de projeto.

---
+++ TresTentativasDeLogin/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string senha, user;
+            int i = 0;
+
+            Console.WriteLine("Digite o usuário: ");
+            user = Console.ReadLine();
+            Console.WriteLine("Digite a senha: ");
+            senha = Console.ReadLine();
+
+
+            if (user == "admin" && senha == "aq123")
+            {
+                Console.WriteLine("Você acessou o sistema.");
+                Console.ReadLine();
+            }
+            else
+            {
+                do
+                {
+                    Console.WriteLine("Dados incorretos. Você tem " + (2 - i) + " tentativas restantes");
+                    Console.WriteLine("Digite o usuário: ");
+                    user = Console.ReadLine();
+                    Console.WriteLine("Digite a senha: ");
+                    senha = Console.ReadLine();
+                    i++;
+                } while (user != "admin" && senha != "aq123" && i <= 1);
+
+                if (user == "admin" && senha == "aq123")
+                {
+                    Console.WriteLine("Você acessou o sistema.");
+                    Console.ReadLine();
+                }
+                else
+                {
+                    Console.WriteLine("Acesso bloqueado");
+                    Console.ReadLine();
+                }
+                
+            }
+
+        }
+    }
+}
